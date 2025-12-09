b36e1868969ffe956f416c73c21be3aa2761668f
Hunter Mann <126185686+huntdamann@users.noreply.github.com>
2025-12-09 09:22:39 -0600

    new scene

--- src/components/threejs/Scene.jsx
+++ src/components/threejs/Scene.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Environment } from '@react-three/drei'
+import { Environment, MeshTransmissionMaterial, AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 import { useRef } from 'react';
 
 function RotatingModel({ progress }) {
@@ -15,22 +15,31 @@ function RotatingModel({ progress }) {
 
   return (
     <mesh ref={modelRef} scale={0.8} position={[0,0.5,0]}>
-      <boxGeometry />
-      <meshStandardMaterial color="orange" />
+    <sphereGeometry args={[1, 32, 32]} />
+  {/* <meshNormalMaterial /> */}
+      <MeshTransmissionMaterial color="#ffffff" roughness={0} metalness={0} thickness={0.5} transmission={1} />
     </mesh>
   );
 }
 
 export default function Scene({ progress }) {
-  return (
+  return 
     <Canvas
       camera={{ position: [0, 0, 5], fov: 45 }}
-      style={{ background: "transparent" }}
+      style={{ background: "blue" }}
+      
     >
       <RotatingModel progress={progress} />
 
-      <directionalLight intensity={5} position={[0, 2, 3]} />
-      <Environment preset="city" />
+      <directionalLight intensity={1} position={[5, 5, 5]} />
+      <ambientLight intensity={Math.PI} />
+      <AccumulativeShadows temporal frames={100} alphaTest={0.9} color="#3ead5d" colorBlend={1} opacity={0.8} scale={20}>
+          <RandomizedLight radius={10} ambient={0.5} intensity={Math.PI} position={[2.5, 8, -2.5]} bias={0.001} />
+      </AccumulativeShadows>
+      {/* <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/dancing_hall_1k.hdr" backgroundBlurriness={1} /> */}
+
+
+
     </Canvas>
   );
 }
